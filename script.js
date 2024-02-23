869ec04d3445e98ea9dcc2a355c75368071140d0
jiajiechen2003 <jchen2223@politecnics.barcelona>
2024-02-23 20:18:49 +0100

    hdcd

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-let pomodoroButton = document.getElementById("pomodoro-button");
-let shortBreakButton = document.getElementById("short-break-button");
-let longBreakButton = document.getElementById("long-break-button");
+const pomodoroButton = document.getElementById("pomodoro-button");
+const shortBreakButton = document.getElementById("short-break-button");
+const longBreakButton = document.getElementById("long-break-button");
+const time = document.getElementById('time');
+let pomodoroTime 
 
 function changeTimer() {
   pomodoroButton.style.borderBottom = "1px solid rgb(177, 177, 177)";
@@ -9,18 +11,21 @@ function changeTimer() {
     shortBreakButton.style.borderBottom = "0px";
     longBreakButton.style.borderBottom = "0px";
     pomodoroButton.style.borderBottom = "1px solid rgb(177, 177, 177)";
+    time.innerHTML = `${25}:${10}`
   });
 
   shortBreakButton.addEventListener("click", function () {
     shortBreakButton.style.borderBottom = "1px solid rgb(177, 177, 177)";
     longBreakButton.style.borderBottom = "0px";
     pomodoroButton.style.borderBottom = "0px";
+    time.innerHTML = `${5}:${10}`
   });
 
   longBreakButton.addEventListener("click", function () {
     shortBreakButton.style.borderBottom = "0px";
     longBreakButton.style.borderBottom = "1px solid rgb(177, 177, 177)";
     pomodoroButton.style.borderBottom = "0px";
+    time.innerHTML = `${15}:${10}`
   });
 }
 
