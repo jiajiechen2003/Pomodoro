1d30117afe7f97b4dd1d2b8402a46442f2adcf53
jiajiechen2003 <jchen2223@politecnics.barcelona>
2024-04-04 17:39:32 +0200

    acabado

--- script.js
+++ script.js
@@ -2,7 +2,10 @@ const pomodoroButton = document.getElementById("pomodoro-button");
 const shortBreakButton = document.getElementById("short-break-button");
 const longBreakButton = document.getElementById("long-break-button");
 const time = document.getElementById("time");
-const pomodoroMinutes = 0.1;
+let pomodoroActive;
+let shortBreakActive;
+let longBreakActive;
+const pomodoroMinutes = 25;
 let pomodoroTime = pomodoroMinutes * 60;
 const shortBreakMinutes = 5;
 let shortBreakTime = shortBreakMinutes * 60;
@@ -22,6 +25,12 @@ function changeBorderBottom() {
   pomodoroButton.style.borderBottom = "0px";
   shortBreakButton.style.borderBottom = "0px";
   longBreakButton.style.borderBottom = "0px";
+}
+
+function changeActiveTimer() {
+  pomodoroActive = false;
+  shortBreakActive = false;
+  longBreakActive = false;
 }
 
 function changeTimerStyle() {
@@ -52,10 +61,14 @@ function changeTimerStyle() {
 
 function changeTimer() {
   pomodoroButton.style.borderBottom = "1px solid rgb(177, 177, 177)";
+  pomodoroActive = true;
   time.innerHTML = formatTime(pomodoroTime);
 
   pomodoroButton.addEventListener("click", function () {
+    startButton.click();
     changeBorderBottom();
+    changeActiveTimer();
+    pomodoroActive = true;
     pomodoroButton.style.borderBottom = "1px solid rgb(177, 177, 177)";
     time.innerHTML = formatTime(pomodoroTime);
     clearInterval(timerInterval);
@@ -63,7 +76,10 @@ function changeTimer() {
   });
 
   shortBreakButton.addEventListener("click", function () {
+    startButton.click();
     changeBorderBottom();
+    changeActiveTimer();
+    shortBreakActive = true;
     shortBreakButton.style.borderBottom = "1px solid rgb(177, 177, 177)";
     time.innerHTML = formatTime(shortBreakTime);
     clearInterval(timerInterval);
@@ -71,7 +87,10 @@ function changeTimer() {
   });
 
   longBreakButton.addEventListener("click", function () {
+    startButton.click();
     changeBorderBottom();
+    changeActiveTimer();
+    longBreakActive = true;
     longBreakButton.style.borderBottom = "1px solid rgb(177, 177, 177)";
     time.innerHTML = formatTime(longBreakTime);
     clearInterval(timerInterval);
@@ -89,11 +108,11 @@ function selectTimerDuration() {
       buttonText.innerHTML = "START";
     } else {
       let selectedDuration;
-      if (pomodoroButton.style.borderBottom !== "0px") {
+      if (pomodoroActive) {
         selectedDuration = pomodoroTime;
-      } else if (shortBreakButton.style.borderBottom !== "0px") {
+      } else if (shortBreakActive) {
         selectedDuration = shortBreakTime;
-      } else if (longBreakButton.style.borderBottom !== "0px") {
+      } else if (longBreakActive) {
         selectedDuration = longBreakTime;
       }
 
@@ -123,5 +142,108 @@ function startTimer(duration) {
 function formatTime(seconds) {
   const minutes = Math.floor(seconds / 60);
   const remainingSeconds = seconds % 60;
-  return `${minutes}:${remainingSeconds < 10 ? "0" : ""}${remainingSeconds}`;
+  return [minutes, remainingSeconds]
+    .map((num) => num.toString().padStart(2, "0"))
+    .join(":");
+}
+
+/* ----------------------------------------------- KANBAN ------------------------------------------ */
+
+const addTaskButton = document.getElementById("add-task");
+const kanbanToDoColumn = document.querySelector(".kanban-column.to-do");
+
+addTaskButton.addEventListener("click", addTask);
+
+function addTask() {
+  let task = document.createElement("div");
+  let taskTitleInput = document.createElement("input");
+  let taskDescriptionTextarea = document.createElement("textarea");
+  let taskTitle = document.createElement("h1");
+  let taskDescription = document.createElement("h2");
+  let confirmButton = document.createElement("button");
+  let deleteButton = document.createElement("button");
+
+  task.id = "task-" + Math.floor(Math.random() * 1000);
+  task.classList.add("task");
+  task.classList.add("draggable");
+  taskTitle.classList.add("task-title");
+  taskDescription.classList.add("task-description");
+  confirmButton.classList.add("btn", "btn-success");
+  deleteButton.classList.add("btn", "btn-danger");
+  task.setAttribute("draggable", "true");
+
+  taskTitleInput.type = "text";
+  taskTitleInput.placeholder = "Title";
+  taskDescriptionTextarea.placeholder = "Description";
+  confirmButton.textContent = "Confirm";
+  deleteButton.textContent = "Delete Task";
+
+  task.appendChild(taskTitleInput);
+  task.appendChild(taskDescriptionTextarea);
+  task.appendChild(confirmButton);
+
+  kanbanToDoColumn.appendChild(task);
+
+  confirmButton.addEventListener("click", function () {
+    if (taskTitleInput.value !== "" && taskDescriptionTextarea.value !== "") {
+      let title = taskTitleInput.value;
+      let description = taskDescriptionTextarea.value;
+
+      taskTitle.textContent = title;
+      taskDescription.textContent = description;
+
+      task.removeChild(taskTitleInput);
+      task.removeChild(taskDescriptionTextarea);
+
+      task.appendChild(taskTitle);
+      task.appendChild(taskDescription);
+      task.appendChild(deleteButton);
+
+      confirmButton.style.display = "none";
+    } else {
+      alert("Enter Text");
+    }
+  });
+
+  deleteButton.addEventListener("click", function () {
+    task.parentNode.removeChild(task);
+  });
+
+  task.addEventListener("dragstart", dragStart);
+  task.addEventListener("dragover", dragOver);
+  task.addEventListener("dragenter", dragEnter);
+  task.addEventListener("dragleave", dragLeave);
+  task.addEventListener("drop", dragDrop);
+}
+
+function dragStart(event) {
+  event.dataTransfer.setData("text/plain", event.target.id);
+}
+
+function dragOver(event) {
+  event.preventDefault();
+}
+
+function dragEnter(event) {
+  event.preventDefault();
+  if (event.target.classList.contains("kanban-column")) {
+    event.target.classList.add("hovered");
+  }
+}
+
+function dragLeave(event) {
+  event.preventDefault();
+  if (event.target.classList.contains("kanban-column")) {
+    event.target.classList.remove("hovered");
+  }
+}
+
+function dragDrop(event) {
+  event.preventDefault();
+  const taskId = event.dataTransfer.getData("text/plain");
+  const draggedTask = document.getElementById(taskId);
+  if (event.target.classList.contains("kanban-column")) {
+    event.target.appendChild(draggedTask);
+    event.target.classList.remove("hovered");
+  }
 }
